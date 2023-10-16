d4d2039ae8188b229cac4b0157a049fc4bc93406
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2023-10-16 10:05:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace tuple_task
+{
+   
+      using System;
+
+class Program
+    {
+        static Tuple<int, int> Sum(int[] numbers)
+        {
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sumEven += num;
+                }
+                else
+                {
+                    sumOdd += num;
+                }
+            }
+
+            return Tuple.Create(sumOdd, sumEven);
+        }
+
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = Sum(numbers);
+
+            Console.WriteLine("tek ededlerin cemi: " + result.Item1);
+            Console.WriteLine("cut ededlerin cemi: " + result.Item2);
+        }
+    }
+
+}
